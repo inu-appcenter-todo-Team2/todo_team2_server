a435824bb93f99f0642085ab902e39024cc8d60d
xunxxoie <ahh0520@naver.com>
2024-12-25 17:45:52 +0900

    [Fix] @Id 어노테이션 라이브러리 변경 - #4

--- src/main/java/org/todo/global/security/jwt/entity/RefreshToken.java
+++ src/main/java/org/todo/global/security/jwt/entity/RefreshToken.java
@@ -1,10 +1,11 @@
 package org.todo.global.security.jwt.entity;
 
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash(value = "refreshToken", timeToLive = 640000)
